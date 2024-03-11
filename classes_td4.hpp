161fba393b56d06e02adf632758d5f370d51a2e3
Sergile Nyouvop <144280076+sn-goat@users.noreply.github.com>
2024-03-11 12:58:27 -0400

    Update classes_td4.hpp

--- classes_td4.hpp
+++ classes_td4.hpp
@@ -1,116 +1,191 @@
 
-#pragma once
-// Structures mémoires pour une collection de films.
+#pragma region "Includes"//{
+#define _CRT_SECURE_NO_WARNINGS // On permet d'utiliser les fonctions de copies de chaînes qui sont considérées non sécuritaires.
 
+#include "classes_td4.hpp"      // Structures de données pour la collection de films en mémoire.
+
+#include "bibliotheque_cours.hpp"
+#include "verification_allocation.hpp" // Nos fonctions pour le rapport de fuites de mémoire.
+
+#include <iostream>
+#include <fstream>
 #include <string>
-#include <memory>
-#include <functional>
-#include <cassert>
+#include <limits>
+#include <algorithm>
+#include <sstream>
+#include <typeinfo>
+#include <iterator>
+#include "cppitertools/range.hpp"
 #include "gsl/span"
-using gsl::span;
+#include "debogage_memoire.hpp"        // Ajout des numéros de ligne des "new" dans le rapport de fuites.  Doit être après les include du système, qui peuvent utiliser des "placement new" (non supporté par notre ajout de numéros de lignes).
 using namespace std;
+using namespace iter;
+using namespace gsl;
 
-class Acteur; class Item;
+#pragma endregion//}
 
+typedef uint8_t UInt8;
+typedef uint16_t UInt16;
+
+#pragma region "Fonctions de base pour lire le fichier binaire"//{
 template <typename T>
-class Liste{
-public:
-	Liste() = default;
-	explicit Liste(int capaciteInitiale) :
-		capacite_(capaciteInitiale),
-		elements_(make_unique<shared_ptr<T>[]>(capacite_))
-	{
+T lireType(istream& fichier)
+{
+	T valeur{};
+	fichier.read(reinterpret_cast<char*>(&valeur), sizeof(valeur));
+	return valeur;
+}
+#define erreurFataleAssert(message) assert(false&&(message)),terminate()
+static const uint8_t enteteTailleVariableDeBase = 0xA0;
+size_t lireUintTailleVariable(istream& fichier)
+{
+	uint8_t entete = lireType<uint8_t>(fichier);
+	switch (entete) {
+	case enteteTailleVariableDeBase+0: return lireType<uint8_t>(fichier);
+	case enteteTailleVariableDeBase+1: return lireType<uint16_t>(fichier);
+	case enteteTailleVariableDeBase+2: return lireType<uint32_t>(fichier);
+	default:
+		erreurFataleAssert("Tentative de lire un entier de taille variable alors que le fichier contient autre chose à cet emplacement.");  //NOTE: Il n'est pas possible de faire des tests pour couvrir cette ligne en plus du reste du programme en une seule exécution, car cette ligne termine abruptement l'exécution du programme.  C'est possible de la couvrir en exécutant une seconde fois le programme avec un fichier films.bin qui contient par exemple une lettre au début.
 	}
-	Liste(const Liste<T>& autre) :
-		capacite_(autre.nElements_),
-		nElements_(autre.nElements_),
-		elements_(make_unique<shared_ptr<T>[]>(nElements_))
-	{
-		for (int i = 0; i < nElements_; ++i)
-			elements_[i] = autre.elements_[i];
-	}
+}
 
-	Liste(Liste<T>&&) = default;
-	Liste<T>& operator= (Liste<T>&&) noexcept = default;
+string lireString(istream& fichier)
+{
+	string texte;
+	texte.resize(lireUintTailleVariable(fichier));
+	fichier.read((char*)&texte[0], streamsize(sizeof(texte[0])) * texte.length());
+	return texte;
+}
 
-	void ajouter(shared_ptr<T> element)
-	{
-		assert(nElements_ < capacite_);
-		elements_[nElements_++] = std::move(element);
-	}
 
-	shared_ptr<T>& operator[] (int index) const { assert(0 <= index && index < nElements_); return elements_[index]; }
-	span<shared_ptr<T>> enSpan() const { return span(elements_.get(), nElements_); }
 
-private:
-	int capacite_ = 0, nElements_ = 0;
-	unique_ptr<shared_ptr<T>[]> elements_;
-};
+#pragma endregion//}
 
-using ListeActeurs = Liste<Acteur>;
 
-class Bibliotheque{
-public:
-    Bibliotheque() = default;
-    Bibliotheque(string nomFichierFilm, string nomFichierLivre);
-    void ajouterItem(unique_ptr<Item> item);
-    void enleverItem(const unique_ptr<Item> item);
-    shared_ptr<Acteur> trouverActeur(const string& nomActeur) const;
-    const vector<unique_ptr<Item>>& obtenirItems();
-    [[nodiscard]] int size() const { return items_.size(); }
-    unique_ptr<Item>& operator[] (int index) { assert(0 <= index && index < size());  return items_[index]; }
-    unique_ptr<Item> trouver(const function<bool(const Item&)>& critere) {
-        for (auto& Item : span(items_))
-            if (critere(*Item))
-                return std::move(Item);
-        return nullptr;
+
+void Bibliotheque::ajouterItem(unique_ptr<Item> item) {
+    items_.push_back(std::move(item));
+}
+
+
+const vector<unique_ptr<Item>>& Bibliotheque::obtenirItems() { return items_; }
+
+void Bibliotheque::enleverItem(const unique_ptr<Item> item) {
+    for (int i : range(size())) {  // Doit être une référence au pointeur pour pouvoir le modifier.
+        if (items_[i] == item) {
+            if (size() > 1)
+                items_.erase(items_.begin() + i);
+            return;
+        }
+    }
+}
+
+
+span<shared_ptr<Acteur>> Film::obtenirActeurs() const{
+    return acteurs_.enSpan();
+}
+
+shared_ptr<Acteur> Bibliotheque::trouverActeur(const string& nomActeur) const
+{
+    for (auto&& item : span(items_)) {
+        for (const shared_ptr<Acteur> &acteur: item->obtenirActeurs()) {
+            if (acteur->obtenirNom() == nomActeur)
+                return acteur;
+
+        }
+    }
+    return nullptr;
+}
+
+
+shared_ptr<Acteur> lireActeur(istream& fichier, const Bibliotheque& bibliotheque)
+{
+    Acteur acteur = {};
+    acteur.nom_            = lireString(fichier);
+    acteur.anneeNaissance_ = int(lireUintTailleVariable (fichier));
+    acteur.sexe_           = char(lireUintTailleVariable(fichier));
+
+    shared_ptr<Acteur> acteurExistant = bibliotheque.trouverActeur(acteur.nom_);
+    if (acteurExistant != nullptr)
+        return acteurExistant;
+    else {
+        cout << "Création Acteur " << acteur.nom_ << endl;
+        return make_shared<Acteur>(std::move(acteur));  // Le move n'est pas nécessaire mais permet de transférer le texte du nom sans le copier.
+    }
+}
+
+
+unique_ptr<Film> lireFilm(istream& fichier, Bibliotheque& bibliotheque)
+{
+    unique_ptr<Film> film = make_unique<Film>();
+    film->titre_       = lireString(fichier);
+    film->realisateur_ = lireString(fichier);
+    film->anneeSortie_ = int(lireUintTailleVariable(fichier));
+    film->recette_     = int(lireUintTailleVariable(fichier));
+    auto nActeurs = int(lireUintTailleVariable(fichier));
+    film->acteurs_ = ListeActeurs(nActeurs);  // On n'a pas fait de méthode pour changer la taille d'allocation, seulement un constructeur qui prend la capacité.  Pour que cette affectation fonctionne, il faut s'assurer qu'on a un operator= de move pour ListeActeurs.
+    cout << "Création Film " << film->titre_ << endl;
+
+    for ([[maybe_unused]] auto i : range(nActeurs)) {  // On peut aussi mettre nElements avant et faire un span, comme on le faisait au TD précédent.
+        film->acteurs_.ajouter(lireActeur(fichier, bibliotheque));
     }
 
+    return film;
+}
+
+unique_ptr<Livre> lireLivre(const string& fichier){
+        unique_ptr<Livre> livre = make_unique<Livre>();
+
+        vector<string> vectorStrings;
+        stringstream ss(fichier);
+        string tmp;
+        while(getline(ss, tmp, '\t'))
+        {
+            tmp.erase(remove(tmp.begin(), tmp.end(), '\"'), tmp.end());
+            vectorStrings.push_back(tmp);
+        }
+
+        livre->titre_ = vectorStrings[0];
+        livre->annee_ = stoi(vectorStrings[1]);
+        livre->auteur_ = vectorStrings[2];
+        livre->millionsCopiesVendues_ = stoi(vectorStrings[3]);
+        livre->nombresPages_ = stoi(vectorStrings[4]);
+        cout << "Création Livre " << livre->titre_ << endl;
+
+        return livre;
+
+}
+
+Bibliotheque::Bibliotheque(string nomFichierFilm, string nomFichierLivre) {
+    ifstream fichierFilm(nomFichierFilm, ios::binary);
+    fichierFilm.exceptions(ios::failbit);
+    int nElements = int(lireUintTailleVariable(fichierFilm));
+
+    for ([[maybe_unused]] int i : range(nElements)) {
+        ajouterItem(lireFilm(fichierFilm, *this));
+    }
+
+    ifstream fichierLivre(nomFichierLivre);
+    string ligne;
+    while(getline(fichierLivre, ligne)) {
+        ajouterItem(lireLivre(ligne));
+    }
+}
 
 
-private:
-    vector<unique_ptr<Item>> items_;
-
-};
-class Item{
-public:
-    Item() = default;
-    virtual ~Item() = default;
-    virtual span<shared_ptr<Acteur>> obtenirActeurs() const {return {};};
-
-private:
-    string Titre, Annee;
-
-
-};
-
-
-class Film : public Item{
-public:
-    friend unique_ptr<Film> lireFilm(istream& fichier, Bibliotheque& Bibliotheque);
-    span<shared_ptr<Acteur>> obtenirActeurs() const override;
-
-private:
-	string titre_, realisateur_;
-	int anneeSortie_=0, recette_=0;
-	ListeActeurs acteurs_;
-};
-
-class Livre : public Item{
-public:
-    friend unique_ptr<Livre> lireLivre(const string& fichier);
-
-private:
-    string titre_, auteur_;
-    int annee_, millionsCopiesVendues_, nombresPages_;
-};
-
-class Acteur
+int main()
 {
-public:
-    friend shared_ptr<Acteur> lireActeur(istream& fichier, const Bibliotheque& bibliotheque);
-    const string& obtenirNom() {return nom_;}
+	#ifdef VERIFICATION_ALLOCATION_INCLUS
+	bibliotheque_cours::VerifierFuitesAllocations verifierFuitesAllocations;
+	#endif
+	bibliotheque_cours::activerCouleursAnsi();  // Permet sous Windows les "ANSI escape code" pour changer de couleurs https://en.wikipedia.org/wiki/ANSI_escape_code ; les consoles Linux/Mac les supportent normalement par défaut.
 
-private:
-	string nom_; int anneeNaissance_=0; char sexe_='\0';
-};
+	static const string ligneDeSeparation = "\n\033[35m════════════════════════════════════════\033[0m\n";
+
+    cout << "Création Bibliothèque" << ligneDeSeparation;
+    Bibliotheque bibliotheque("films.bin", "livres.txt");
+
+
+
+
+}
